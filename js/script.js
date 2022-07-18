3877ad6c55db21f556e9190854bfe6c63e352d25
Digoh-Oliveira <digoh.psn@hotmail.com>
2022-07-18 10:40:19 -0300

    :package: atualizando a pagina index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 document.querySelector('.hamburguer').addEventListener('click', () => 
 
     document.querySelector('.container').classList.toggle('show-menu')
-
 );
+//script para o scroll descer até ao setor Conhecimentos e subir para a page Home
+const menuItem = document.querySelectorAll('.menu a[href^="#"]');
+menuItem.forEach(item => {
+    
+    item.addEventListener('click', scrollToId);
+})
+
+function scrollToId(event){
+    
+    event.preventDefault();
+    
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+    
+    window.scroll({
+        
+        top:to,
+        behavior: "smooth"});
+    
+    document.querySelector('.show-menu').classList.toggle('hidden-menu');
+    
+    document.querySelector('.hamburguer').addEventListener('click', () => 
+    document.querySelector('.container').classList.remove('hidden-menu'), document.querySelector('.container').classList.toggle('show-menu'))
+}
+
+
+
